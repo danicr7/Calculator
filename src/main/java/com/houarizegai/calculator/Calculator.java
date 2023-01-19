b76d9069d0c741da887e3bf15aa5a74a4cbef2bc
danicr7 <122361525+danicr7@users.noreply.github.com>
2023-01-19 11:17:01 -0800

    made changes

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -11,7 +11,7 @@ import javax.swing.*;
 import java.lang.Math;
 
 public class Calculator {
-//commiting new changes
+//commit new changes for the project
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
